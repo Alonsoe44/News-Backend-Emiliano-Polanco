ee7133eb80084676a999f7c9dbfd6f2fa0e64e4b
Emiliano Polanco Baquerizo <emilianopolanco5@gmail.com>
2022-07-13 22:57:26 +0200

    Creating error handling and getNews endpoint

--- src/server/index.ts
+++ src/server/index.ts
@@ -4,7 +4,12 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import express from "express";
+import { internatServerError, notFoundError } from "./middlewares/errors";
+import newsRouter from "./routers/newsRouter";
 
 const app = express();
 
+app.use("/news", newsRouter);
+app.use(notFoundError);
+app.use(internatServerError);
 export default app;
