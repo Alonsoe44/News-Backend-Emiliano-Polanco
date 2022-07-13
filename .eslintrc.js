56b98791a0dad4333c3b85c7fab5f1986765dedd
Emiliano Polanco Baquerizo <emilianopolanco5@gmail.com>
2022-07-13 12:29:48 +0200

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: ["airbnb-base", "prettier"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+
+  settings: {
+    "import/resolver": {
+      node: {
+        extensions: [".ts", ".tsx", ".js", ".jsx"],
+      },
+    },
+  },
+};
